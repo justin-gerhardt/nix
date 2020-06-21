bf375b1788df22f8fc67e7062f4e5d7cb81ca771
Justin Gerhardt <justin@gerhardt.link>
2020-06-21 14:06:42 -0400

    Hoist kdeconnect wrapper
    So It doesn't have to be rebuilt from source

--- user/base/kdeconnect.nix
+++ user/base/kdeconnect.nix
@@ -2,14 +2,19 @@
 
   # adding xdg-open and it's depends to path so that kdeconnect can actually open things
   nixpkgs.config.packageOverrides = pkgs: {
-    kdeconnect = pkgs.kdeconnect.overrideAttrs (old: rec {
-      postInstall = old.postInstall + ''
-        wrapProgram $out/libexec/kdeconnectd --prefix PATH : ${
-          lib.makeBinPath [ pkgs.xdg_utils pkgs.coreutils-full pkgs.which ]
-        }
-      '';
-    });
+    kdeconnect =
+      pkgs.symlinkJoin {
+        name = "kdeconnect";
+        paths = [ pkgs.kdeconnect ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/libexec/kdeconnectd --prefix PATH : ${
+            lib.makeBinPath [ pkgs.xdg_utils pkgs.coreutils-full pkgs.which ]
+          }
+        '';
+      };
   };
+
   systemd.user.services.kdeconnect.Unit.Requires = [ "dbus.service" ];
   services.kdeconnect.enable = true;
 
