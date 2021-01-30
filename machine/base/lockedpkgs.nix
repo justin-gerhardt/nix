c1afb236a8c4aec43264ab411ef1cafb39f4ecf3
Justin Gerhardt <justin@gerhardt.link>
2021-01-30 16:37:41 -0500

    lock polybar pkgs

--- machine/base/lockedpkgs.nix
+++ machine/base/lockedpkgs.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, overlays ? [], ... }: {
   # used for patched software the can update upstream
   modifiedPkgs = (import (pkgs.fetchFromGitHub {
     owner = "NixOS";
@@ -6,7 +6,7 @@
     # nixos-unstable-small as of Sun 10 Jan 2021 01:54:00 AM EST
     rev = "97028f053c08fea03a64da70d6ae95199d1c7983";
     sha256 = "1g5pb9yv9znvy1fpp2v52785nlfnchwfzi6fjdz4z1wd1jpanraf";
-  }) { config.allowUnfree = true; });
+  }) { config.allowUnfree = true; inherit overlays; });
   # used for my custom builds that won't update upstream
   newPkgs = (import (pkgs.fetchFromGitHub {
     owner = "NixOS";
@@ -14,5 +14,5 @@
     # nixos-unstable-small as of Sun 10 Jan 2021 01:54:00 AM EST
     rev = "97028f053c08fea03a64da70d6ae95199d1c7983";
     sha256 = "1g5pb9yv9znvy1fpp2v52785nlfnchwfzi6fjdz4z1wd1jpanraf";
-  }) { config.allowUnfree = true; });
+  }) { config.allowUnfree = true; inherit overlays;});
 }
