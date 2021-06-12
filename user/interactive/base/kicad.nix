847cb5c132411c4bca9d7de27fa3242db4816580
Justin Gerhardt <justin@gerhardt.link>
2021-06-12 01:41:02 -0400

    update kicad

--- user/interactive/base/kicad.nix
+++ user/interactive/base/kicad.nix
@@ -1,15 +1,8 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.kicad
-    (pkgs.makeDesktopItem {
-      name = "KiCad";
-      exec = "kicad";
-      comment = "Electronic Design Automation suite";
-      desktopName = "KiCad";
-      genericName = "EDA Suite";
-      categories = "Development;Electronics;";
-      mimeType = "application/x-kicad-project;";
-    })
+    pkgs.libxslt
+
   ];
 }
 
